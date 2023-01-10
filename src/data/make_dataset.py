b1b673a45b942d6b4f9945f62f9fdbffe5f7b2e8
Jakob Fahl <s184419@student.dtu.dk>
2023-01-10 14:57:20 +0100

    make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,18 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import torch
+import numpy as np
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+content = [ ]
+for i in range(5):
+    content.append(np.load(f"data/raw/train_{i}.npz", allow_pickle=True))
+train = torch.tensor(np.concatenate([c['images'] for c in content])).reshape(-1, 1, 28, 28)
+train_targets = torch.tensor(np.concatenate([c['labels'] for c in content]))
+
+torch.save(train, 'data/processed/train.npz')
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+content = np.load("data/raw/test.npz", allow_pickle=True)
+test = torch.tensor(content['images']).reshape(-1, 1, 28, 28)
+test_targets = torch.tensor(content['labels'])
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+torch.save(test, 'data/processed/test.npz')
