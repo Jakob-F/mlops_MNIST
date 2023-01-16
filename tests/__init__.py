7d1908b082242303cfe9a3c0793379119ce2c5ed
Jakob Fahl <s184419@student.dtu.dk>
2023-01-16 17:58:10 +0100

    updated requirements.txt

--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-
-_TEST_ROOT = os.path.dirname('C:\Users\Administrator\Desktop\MLOps GitHub\mlops_MNIST\tests')  # root of test folder
-_PROJECT_ROOT = os.path.dirname('C:\Users\Administrator\Desktop\MLOps GitHub\mlops_MNIST')  # root of project
-_PATH_DATA = os.path.join('C:\Users\Administrator\Desktop\MLOps GitHub\mlops_MNIST\data', "Data")  # root of data
