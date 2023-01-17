53b82b89478d7608b4ca9d08cd663ae0f8d7e41c
Jakob Fahl <s184419@student.dtu.dk>
2023-01-17 14:56:58 +0100

    added tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,6 @@ import os
 
 
 _TEST_ROOT = os.path.dirname(__file__)  # root of test folder
-_TEST_ROOT = _TEST_ROOT[:-6]
+_TEST_ROOT = _TEST_ROOT[:-18]
 _PROJECT_ROOT = os.path.join(_TEST_ROOT, "src")  # root of project
 _PATH_DATA = os.path.join(_TEST_ROOT, "data/processed")  # root of data
